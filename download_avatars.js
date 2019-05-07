edc8bdc340b6360f062b4b7100495bb0539f5d62
Rebecca Lisl Gold <rebecca.gold@mail.mcgill.ca>
2019-05-07 00:55:15 +0000

    Clean up before submitting project

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var secrets = require('./secrets')
+var secrets = require('./secrets');
 var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -18,23 +18,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-var repo_Name = process.argv[2]
-var repo_Owner = process.argv[3]
+var repo_Name = process.argv[2];
+var repo_Owner = process.argv[3];
 
 getRepoContributors(repo_Owner, repo_Name, function(err, result) {
 
   if(repo_Name && repo_Owner) {
     console.log("Errors:", err);
-    // console.log("Result:", result);
     var obj = JSON.parse(result);
 
     for (var index in obj) {
       var avatarUrl = obj[index].avatar_url;
-      var path = "avatars/" + obj[index].login + ".jpg"
-      downloadImageByURL(avatarUrl, path)
+      var path = "avatars/" + obj[index].login + ".jpg";
+      downloadImageByURL(avatarUrl, path);
     }
+
   } else {
-    console.log("Please enter a repository name and owner.")
+    console.log("Please enter a repository name and owner.");
   }
 });
 
@@ -44,17 +44,8 @@ function downloadImageByURL(url, filePath) {
        .on('error', function (err) {
          throw err;
        })
-      //  .on('response', function (response) {
-      //    console.log('Downloading image...', 'Response Status Code: ', response.statusCode, "; Response Message: ", response.statusMessage, "; Content Type: ", response.headers['content-type']);
-      //  })
-      // .on('end', function (response) {
-      //   console.log('Download complete!')})
       .pipe(fs.createWriteStream(filePath));
-
 }
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
-
 
 
 
